adc7a9bd5ce4ea44c143e9ac24757dca902414c1
Julia Link <juliazinalink@gmail.com>
2014-02-22 14:23:54 +0000

    improved search to accept variable as keyword

--- lib/search.py
+++ lib/search.py
@@ -2,27 +2,31 @@
 from database import *
 
 class Search:
+    '''enables various searches of the database'''
 
     def __init__(self):
-        self.connection = getConnection()
+        self.connection = getDictConnection()
 
     def setTerm(self, term):
         self.term=term
+        self.term= "%" + self.term + "%"
 
     def getTerm(self):
         return self.term
+    
+    def reveal(self):
+        print "Term is: %s" % self.term
 
     def searchTitle(self):
         '''accepts keyword 'term', searches titles
         returns titles and associated info. of books that have term in the title
         '''
-        self.term = term
 
         sql='''select title, book.book_id, author                                     
         from book, book_author, author                                                
-        where title like '%demon%' and                                                
+        where title like "%s" and                                                
         book.book_id=book_author.book_id and                                          
-        author.author_id=book_author.author_id;'''
+        author.author_id=book_author.author_id;''' % self.term
 
         output=execute(self.connection, sql)
         return output
@@ -32,8 +36,6 @@ if __name__ == '__main__':
 #test code                                                                            
     test= Search()
     test.setTerm('witch')
-    TERM = test.getTerm()
+    test.reveal()
     result= test.searchTitle()
-    print TERM
     print result
-
