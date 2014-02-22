f3a52faa3c072e5f3148972c41b96391c0b4eb1d
Julia Link <juliazinalink@gmail.com>
2014-02-22 14:46:28 +0000

    connected webpage to Search class enabling interactive functionality

--- web/title_search.py
+++ web/title_search.py
@@ -6,10 +6,12 @@ cgitb.enable()
 #enables colored error messages to appear in browser.                          
 
 from search import Search
+#imports methods from class Search
 
 form = cgi.FieldStorage()
 
 term= form.getvalue('term', 'not entered')
+#sets term to 'term'  or else defaults 'term' to not entered 
 
 print "Content-type: text/html\n"
 print "<html>"
@@ -19,7 +21,14 @@ print """
 Search Titles For: <input type='text' name = 'term'/>
 <input type =  'submit' />
 </form>"""
+#generates from that accepts keyword search term
 
-print "<h3> Search term is: %s</h3>" %term
+print "<h3> Search Term Is: %s</h3>" %term
+
+titles = Search()
+titles.setTerm(term)
+result=titles.searchTitle()
+
+print result
 print "</html>"
 
