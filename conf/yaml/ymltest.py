22ecbac714ed6b202a53ac17e3bc36ac87a67be2
Julia Link <juliazinalink@gmail.com>
2014-06-04 04:37:45 +0000

    cleaned up directories, made three yaml files: columns, Outputtables, DBtables with new schema

---
+++ conf/yaml/ymltest.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import yaml
+import pprint
+
+#for data in yaml.load(y):
+    #pprint.pprint(data)
+
+#people= yaml.load(y)
+#pprint.pprint(people)
+#for person in people:
+#    print person['name']
+
+stuff= yaml.load(open('stuff.yml'))
+#pprint.pprint(stuff)
+
+for d in stuff['folks']:
+    for k, v in d:
+        if k == 'color':
+            print v
+
