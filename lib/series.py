54fe317a804ae48be2c060ab2ea5bbc94c9bae93
Julia Link <juliazinalink@gmail.com>
2015-03-21 02:12:07 -0400

    write update series method

--- lib/series.py
+++ lib/series.py
@@ -29,14 +29,18 @@ class Series:
     def addNewSeries(self, series):
         '''accepts a series name
         adds this name to the series table
-        returns sucess message'''
+        returns series_id of new series name'''
         
         sql = '''insert into series (series)
                  values ('%s')''' % (series)
 
         addtion = execute(self.connection, sql)
         
-        return "Series %s  added to Database" %(series)
+        IDsql = "select series_id from series where series = ('%s')" %series
+        results = execute(self.connection, IDsql)
+        series_id = results[0]['series_id']
+        
+        return series_id
 
     def getAsAutoCDict(self):
         '''Return all series records as a dict with key, values
@@ -48,14 +52,51 @@ class Series:
 
         return results
 
-    def updateSeries(self, var):
-        pass
+    def updateSeries(self, formDict):
+        ''' update series by checking if the given series is in the DB yet,
+        adding it if it is not, then adding the series to the book, return
+        success message'''
+        #get series_id
+        series_id = formDict['series_id']
+        series_name = formDict['series']
+       
+        #if there is no series ID and there is a series name one of two things
+        #could be happening: 1- the series was the default name (already
+        #assigned to the book), in which case do nothing OR the series name is
+        # new and needs to be added to the DB, in which case add it
+
+        #check if series name is in the DB or not
+        if series_id == '' and series_name !='':
+            sql = 'select series_id from series where series = ("%s")'\
+                %series_name
+            results = execute(self.connection, sql)
+            if results:
+                series_id = results[0]['series_id']
+            else:
+               series_id =  self.addNewSeries(series_name)
+
+        #check if the series_id and book_id are already paired
+        sql = 'select series_id from book where book_id = ("%s")'\
+            %formDict['book_id']
+        results = execute(self.connection, sql)
+
+        #if they are pass, else add them.
+        if results[0]['series_id'] == series_id:
+            pass
+        else:
+            sql = 'update book set series_id = ("%s") where book_id = ("%s")'\
+                %(series_id, formDict['book_id'])
+            
+            results = execute(self.connection, sql)
+            x = 'updated series_id in book table'
+        return 'update activate'
 
 def test():
     test = Series()
-    getSeries = test.getSeries(1)
-    addSeries = test.addNewSeries('Millennium Series')
-    print addSeries
+    testDict = {'book_id': 522, 'series_id':'', 'series_name': 'Twelve Houses'}
+#    getSeries = test.getSeries(1)
+#    addSeries = test.addNewSeries('Millennium Series')
+    print test.updateSeries(testDict)
 
 if __name__ == '__main__':
     test()
